35202d977d92d886dbc685441930a99f8896018d
ilya_popov <il_pop@mail.ru>
2023-11-05 11:09:58 +0300

    done 4.8.4

---
+++ file4_8_4.py
@@ -0,0 +1,29 @@
+from file4 import GetHtml, Parser
+
+url = 'https://parsinger.ru/table/4/index.html'
+
+html = GetHtml(url=url).get_html()
+
+
+class MyParser(Parser):
+    def _get_date_in_row(self, row) -> list:  # get list of date in one row
+        res = row.find_all(name='td', attrs={'class': 'green'})
+        res = list(map(lambda x: x.text, res))
+        return res
+
+    def run(self):
+        result = 0
+        rows = self._rows()[1:]
+        for row in rows:
+            list_row = self._get_date_in_row(row)
+            res = sum(map(float, list_row))
+            result += res
+        print(result)
+
+
+def mane():
+    MyParser(html=html).run()
+
+
+if __name__ == "__main__":
+    mane()
