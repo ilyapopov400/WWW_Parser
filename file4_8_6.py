e522c193b406b7fbe1b91295531d1ac9183f07ec
ilya_popov <il_pop@mail.ru>
2023-11-06 09:54:52 +0300

    done 4.8.60

---
+++ file4_8_6.py
@@ -0,0 +1,26 @@
+from file4 import GetHtml, Parser
+
+url = 'https://parsinger.ru/table/5/index.html'
+
+html = GetHtml(url=url).get_html()
+
+
+class MyParser(Parser):
+    def run(self):
+        result = dict()
+        rows = self._rows()[1:]
+        for row in rows:
+            list_row = self._get_date_in_row(row)
+            for e, num in enumerate(list_row):
+                key = '{} column'.format(e+1)
+                val = result.get(key, 0)
+                result[key] = round((val + float(num)), 3)
+        print(result)
+
+
+def mane():
+    MyParser(html=html).run()
+
+
+if __name__ == "__main__":
+    mane()
