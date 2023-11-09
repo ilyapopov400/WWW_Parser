662c0aa445b20e679088e0c5a0223954f51cade5
ilya_popov <il_pop@mail.ru>
2023-11-09 08:42:10 +0300

    class GetHtml in file get_html.py

---
+++ parser_lesson4_9/get_html.py
@@ -0,0 +1,70 @@
+import requests
+from fake_useragent import UserAgent
+import os
+
+
+class GetHtml:
+    '''
+    получаем html в виде строки с использованием UserAgent
+    '''
+
+    def __init__(self, url):
+        self.url = url
+        self.path = os.getcwd()
+        self.directori = 'datas_dir'  # имя директории и файла для хранения html
+        self.filename = url.split('/')[-1]
+
+    def _fake_user_agent(self):  # crate fake user agent
+        ua = UserAgent()
+        fake_ua = {'user-agent': ua.random}
+        return fake_ua
+
+    def _create_save_file(self):
+        if not os.path.exists('{}/{}'.format(self.path, self.directori)):
+            os.mkdir('{}/{}'.format(self.path, self.directori))
+
+        if os.path.exists('{}/{}/{}'.format(self.path, self.directori, self.filename)):
+            return True
+        else:
+            return False
+
+    def _response_html(self) -> str:
+        header = self._fake_user_agent()
+        response = requests.get(url=self.url, headers=header)
+        response.encoding = 'utf-8'
+        if response:
+            print(response.status_code)
+            return response.text
+        else:
+            print('ERROR {}'.format(response.status_code))
+            return False  # если страницы нет
+
+    def get_html(self):
+        if self._create_save_file():  # если файл существует, читаем с него данные
+            with open(file='{}/{}/{}'.format(self.path, self.directori, self.filename), mode='r') as f:
+                html = f.read()
+        if True is False:
+            pass
+        else:  # если файла нет, вызываем _response_html и пишем его в файл
+            html = self._response_html()
+            if not html:
+                return False  # если страницы нет
+            with open(file='{}/{}/{}'.format(self.path, self.directori, self.filename), mode='w') as f:
+                f.write(html)
+        return html
+
+
+def mane():
+    url = 'https://parsinger.ru/html/index4_page_{}.html'
+    num = 1
+    while True:
+        url_page = url.format(num)
+        html = GetHtml(url=url_page).get_html()
+        if not html:
+            break
+        print(num)
+        num += 1
+
+
+if __name__ == "__main__":
+    mane()
